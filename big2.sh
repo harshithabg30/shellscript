812dfc1168e26605e4ef3eb497a7ad33712e6751
Cloud User <ec2-user@ip-172-31-5-56.ap-south-1.compute.internal>
2023-07-10 10:13:14 +0000

    added big2.sh

---
+++ big2.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+if [ $1 -eq $2 ];then
+echo "The numbers are equal"
+
+elif [ $1 -gt $2 ];then
+echo "$1 is the biggest of the two numbers"
+
+else
+echo "$2 is the biggest of the two numbers"
+fi
